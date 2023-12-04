e9287aa7ba74b8f5892fc991e3a5f1ac8154dcb4
Jakub Filipowicz <jakubf@gmail.com>
2023-12-04 18:22:56 +0100

    disktest

--- disktest/dt.asm
+++ disktest/dt.asm
@@ -5,11 +5,11 @@
 	.include io.inc
 
 	uj	start
-
+status:	.res	1
 imask:	.word	IMASK_ALL & ~(IMASK_CPU_H | IMASK_GROUP_L)
 
 dummy:	lip
-
+marker:	.word	666
 stack:	.res	11*4, 0x0ded
 
 	.org	INTV
@@ -39,34 +39,42 @@ floptest:
 	rw	r7, .regs
 	lw	r7, r6
 	srz	r7
+	rz	status
 
 	rndfill(wrbuf, r7)
 	memset(rdbuf, r7, 0)
 
 	; write data
 
+	ib	status	; 1
 	kz_wrseek(r5, [FLOP])
 	cwt	r1, 0
 	jn	.done
+	ib	status	; 2
 	write(wrbuf, [FLOP], r6)
 	cwt	r1, 0
 	jn	.done
+	ib	status	; 3
 	kz_detach([FLOP])
 	cwt	r1, 0
 	jn	.done
 
 	; read data
 
+	ib	status	; 4
 	kz_seek(r5, [FLOP])
 	cwt	r1, 0
 	jn	.done
+	ib	status	; 5
 	read(rdbuf, [FLOP], r6)
 	cwt	r1, 0
 	jn	.done
+	ib	status	; 6
 	kz_detach([FLOP])
 	cwt	r1, 0
 	jn	.done
 
+	ib	status	; 7
 	memcmp(rdbuf, wrbuf, r7)
 .done:
 	lw	r7, [.regs]
@@ -88,8 +96,14 @@ testv:
 ; ------------------------------------------------------------------------
 
 start:
+	lw	r7, [marker]
+	cwt	r7, 0
+	jes	.skip_rky
 	rky	r7
 	rw	r7, FLOP
+.skip_rky:
+	lw	r7, 0
+	rw	r7, marker
 	lj	tmrandom
 	lj	seed
 	lw	r7, [FLOP]
@@ -97,7 +111,7 @@ start:
 	nr	r7, 0b1111
 	kz_init(r7)
 	im	imask
-
+.again:
 	lw	r7, testv
 .loop:
 	lw	r5, [r7]
@@ -106,17 +120,19 @@ start:
 	lw	r6, [r7+1]
 	lj	floptest
 	cwt	r1, 0
-	jn	err
+	jn	.err
 	awt	r7, TEST_VEC_SIZE
 	ujs	.loop
+
 .done:
 	kz_reset([FLOP])
+	hlt	0
+	ujs	-2
 
-halt:	hlt
-	ujs	halt
-
-err:	hlt	1
-	ujs	err
+.err:
+	kz_reset([FLOP])
+	hlt	1
+	ujs	-2
 
 	.equ	wrbuf .
 	.equ	rdbuf wrbuf + 4*128
