d33804172072b4679458466e747a0dd6e8aee427
Jakub Filipowicz <jakubf@gmail.com>
2026-04-24 17:56:34 +0200

    t64: make configurable, lower footprint

--- t64/t64.asm
+++ t64/t64.asm
@@ -1,78 +1,128 @@
 	.cpu mera400
 
-; test 64k COMPUTEX memory module
+; test <= 64K system memory (Elwro or Computex):
 
 	.include cpu.inc
 	.include io.inc
 
-	mcl
-	uj	start
-
 	.const	MAGIC1 0b1010101000000000
 	.const	MAGIC2 0b0101010100000000
 
-imask:	.word	IMASK_ALL_MEM | 0\SR_NB
+	mcl
+
+	; read from keys and store the number of memory pages to test (>=3 needed)
+	rky	r1
+	cwt	r1, 3
+	jls	bad_page_cnt
+	rw	r1, page_cnt
+	; calculate and store last memory test address
+	shc	r1, -12
+	awt	r1, -1
+	rw	r1, mem_test_end_addr
+
+	uj	start
+
+bad_page_cnt:
+	hlt	1
+
+imask:	.word	IMASK_PARITY | IMASK_NOMEM | 0\SR_NB
+page_cnt:
+	.res	1
+mem_test_end_addr:
+	.res	1
 
 ; ------------------------------------------------------------------------
-mem_int:
+int_mem_parity:
 	hlt	044
-	ujs	mem_int
+int_mem_segfault:
+	hlt	045
+int_unexpected:
+	hlt	046
 
 ; ------------------------------------------------------------------------
 test_addr:
 	.res	1
-	lw	r4, mem_start
+	lw	r4, mem_test_start_addr
 .wrloop:
 	rw	r4, r4
-	irb	r4, .wrloop
+	cl	r4, [mem_test_end_addr]
+	jes	.do_reads
+	awt	r4, 1
+	ujs	.wrloop
 
-	lw	r4, mem_start
+.do_reads:
+	lw	r4, mem_test_start_addr
 .rdloop:
 	cw	r4, [r4]
 	jes	.cont
 	hlt	076
-.cont:	irb	r4, .rdloop
+.cont:	cl	r4, [mem_test_end_addr]
 	je	[test_addr]
+	awt	r4, 1
+	ujs	.rdloop
 
 ; ------------------------------------------------------------------------
-unexpected_int:
+march_seq:
+	.word	march_up_w0
+	.word	march_up_r0_w1
+	.word	march_up_r1_w0
+	.word	march_dn_r0_w1
+	.word	march_dn_r1_w0
+	.word	march_up_r0
+march_seq_end:
+
+; ------------------------------------------------------------------------
+; uses: r7
+test_march:
+	.res	1
+	lw	r7, march_seq
+.loop:
+	lj	[r7]
+	awt	r7, 1
+	cw	r7, march_seq_end
+	jn	.loop
+	uj	[test_march]
+
+; ------------------------------------------------------------------------
+fail:
 	hlt	077
-	ujs	unexpected_int
 
 ; ------------------------------------------------------------------------
-
-	; squeeze as much as possible before the interrupt vector table
 
 	.org	INTV
-	.res	32, unexpected_int
+	.res	1, int_unexpected
+	.res	1, int_mem_parity
+	.res	1, int_mem_segfault
+	.res	29, int_unexpected
+	.org	STACKP
+	.res	1, mem_test_start_addr ; don't care if stack overwrites test area (test failed anyway)
+
 	.org	OS_START
 
 ; ------------------------------------------------------------------------
 conf_all:
 	.res	1
-	lwt	r2, 2		; starting page number
+	lwt	r2, 2			; starting page number
 .next:
 	lw	r1, r2
-	shc	r1, 4		; move bits to the page number position, segment is always 0
+	shc	r1, -12			; move bits to the page number position, segment is always 0
 
 	lw	r3, r2
-	shc	r3, 11			; move bits to the frame number position
+	shc	r3, -5			; move bits to the frame number position
 	nr	r3, 0b0000000011100000	; lower bits - frame number
 	lw	r4, r2
 	shc	r4, 2			; move bits to the memory module number position
 	nr	r4, 0b0000000000000010	; highest bit - memory module number
 	aw	r3, r4
-	aw	r3, MEM_CFG
 
-	ou	r1, r3
+	ou	r1, r3 + MEM_CFG
 	.word	.no, .en, .ok, .pe
 .no:	hlt	010
 .en:	hlt	011
 .pe:	hlt	012
-
 .ok:
 	awt	r2, 1
-	cwt	r2, 16
+	cw	r2, [page_cnt]
 	je	[conf_all]
 	ujs	.next
 
@@ -97,7 +147,6 @@ frame_marks:
 .no:	hlt	020
 .en:	hlt	021
 .pe:	hlt	022
-
 .ok:
 	; store the frame marker
 	lw	r1, MAGIC1 + r2
@@ -106,7 +155,7 @@ frame_marks:
 	rw	r1, 0x2001
 
 	awt	r2, 1
-	cwt	r2, 16
+	cw	r2, [page_cnt]
 	je	[frame_marks]
 
 	; "deconfigure" page
@@ -145,7 +194,6 @@ test_mapping:
 .no:	hlt	040
 .en:	hlt	041
 .pe:	hlt	042
-
 .ok:
 	; check frame marker
 	rw	r6, .nb
@@ -169,26 +217,28 @@ test_mapping:
 .ok2:
 	lw	r1, [.t1]
 	awt	r2, 1
-	cwt	r2, 16
+	cw	r2, [page_cnt]
 	jes	.seg_done
 	ujs	.next_frame
 
 .seg_done:
 	lwt	r2, 2	; starting frame number
 	awt	r5, 1
-	cwt	r6, 15	; is this the last segment?
+	md	[page_cnt]
+	cwt	r6, -1	; is this the last segment?
 	jes	.last_seg
-	cwt	r5, 16	; test the last page
+	cw	r5, [page_cnt]	; test the last page
 	jes	.next_segment
 	uj	.next_page
 .last_seg: ; for last segment, don't test last page
-	cwt	r5, 15
+	md	[page_cnt]
+	cwt	r5, -1
 	jes	.next_segment
 	uj	.next_page
 .next_segment:
 	lwt	r5, 0
 	awt	r6, 1
-	cwt	r6, 16
+	cw	r6, [page_cnt]
 	je	.done
 	uj	.next_page
 
@@ -206,9 +256,14 @@ test_mapping:
 ; ------------------------------------------------------------------------
 march_up_w0:
 	.res	1
-	lw	r1, mem_start
 
-.loop:	rz	r1
+	lw	r1, mem_test_start_addr-1
+	lw	r4, [mem_test_end_addr]
+	sw	r1, r4
+	awt	r4, 1
+	lwt	r2, 0
+.loop:
+	rz	r4+r1
 	irb	r1, .loop
 
 	uj	[march_up_w0]
@@ -216,11 +271,15 @@ march_up_w0:
 ; ------------------------------------------------------------------------
 march_up_rw:
 	.res	1
-	lw	r1, mem_start
 
-.loop:	cw	r2, [r1]
+	lw	r1, mem_test_start_addr-1
+	lw	r4, [mem_test_end_addr]
+	sw	r1, r4
+	awt	r4, 1
+.loop:
+	cw	r2, [r4+r1]
 	jn	fail
-	rw	r3, r1
+	rw	r3, r4+r1
 	irb	r1, .loop
 
 	uj	[march_up_rw]
@@ -228,108 +287,86 @@ march_up_rw:
 ; ------------------------------------------------------------------------
 march_up_r0_w1:
 	.res	1
+
 	lwt	r2, 0
 	lwt	r3, -1
 	lj	march_up_rw
+
 	uj	[march_up_r0_w1]
 
 ; ------------------------------------------------------------------------
 march_up_r1_w0:
 	.res	1
+
 	lwt	r2, -1
 	lwt	r3, 0
 	lj	march_up_rw
+
 	uj	[march_up_r1_w0]
 
 ; ------------------------------------------------------------------------
 march_dn_rw:
 	.res	1
-	lw	r1, 0xffff - (mem_start-1)
-	lw	r4, mem_start-1
 
-.loop:	cw	r2, [r1+r4]
+	lw	r4, mem_test_start_addr-1
+	lw	r1, [mem_test_end_addr]
+	sw	r1, r4
+.loop:
+	cw	r2, [r4+r1]
 	jn	fail
-	rw	r3, r1+r4
+	rw	r3, r4+r1
 	drb	r1, .loop
+
 	uj	[march_dn_rw]
 
 ; ------------------------------------------------------------------------
 march_dn_r0_w1:
 	.res	1
+
 	lwt	r2, 0
 	lwt	r3, -1
 	lj	march_dn_rw
+
 	uj	[march_dn_r0_w1]
 
 ; ------------------------------------------------------------------------
 march_dn_r1_w0:
 	.res	1
+
 	lwt	r2, -1
 	lwt	r3, 0
 	lj	march_dn_rw
+
 	uj	[march_dn_r1_w0]
 
 ; ------------------------------------------------------------------------
 march_up_r0:
 	.res	1
-	lw	r1, mem_start
-	lwt	r2, 0
 
-.loop:	cw	r2, [r1]
+	lw	r1, mem_test_start_addr-1
+	lw	r4, [mem_test_end_addr]
+	sw	r1, r4
+	awt	r4, 1
+	lwt	r2, 0
+.loop:
+	cw	r2, [r4+r1]
 	jn	fail
 	irb	r1, .loop
+
 	uj	[march_up_r0]
 
 ; ------------------------------------------------------------------------
-march_seq:
-	.word	march_up_w0
-	.word	march_up_r0_w1
-	.word	march_up_r1_w0
-	.word	march_dn_r0_w1
-	.word	march_dn_r1_w0
-	.word	march_up_r0
-march_seq_end:
-
-; ------------------------------------------------------------------------
-; uses: r7
-test_march:
-	.res	1
-	lw	r7, march_seq
-.loop:
-	lj	[r7]
-	awt	r7, 1
-	cw	r7, march_seq_end
-	jn	.loop
-	uj	[test_march]
-
-; ------------------------------------------------------------------------
-fail:
-	hlt	046
-
+; --- MAIN ---------------------------------------------------------------
 ; ------------------------------------------------------------------------
 start:
-	; prepare interrupt vectors
-	lw	r1, mem_start
-	rw	r1, STACKP
-	lw	r1, mem_int
-	rw	r1, INTV_PARITY
-	rw	r1, INTV_NOMEM
 	im	imask
 
-	; mark all frames
-	lj	frame_marks
-
-	; run mapping test
-	lj	test_mapping
-
-	; configure all OS memory: 64k, 2 modules 32k each
-	lj	conf_all
-
-	; run addressing test
-	lj	test_addr
-
+	lj	frame_marks	; mark all frames
+	lj	test_mapping	; run mapping test
+	lj	conf_all	; configure all pages into OS memory segment
+	lj	test_addr	; run addressing test
 .loop:
-	lj	test_march
-ujs	.loop
+	lj	test_march	; run MARCH C- test in an infinite loop
+	ujs	.loop
 
-mem_start:
+mem_test_start_addr:
